b264e2965965231a8c4660ffd5c7e71377eb4cde
alexpizaiot <alexpizaiot@gmail.om>
2025-01-03 00:02:42 -0300

    dasboard e enviar doc 1.0

--- dashboard.js
+++ dashboard.js
@@ -20,24 +20,7 @@ import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/10.7.1/fire
      const REDIRECT_URI = 'https://portal-docentes.vercel.app/oauth2callback'
         let tokenClient;
         let gapiInited = false;
-   // Initialize the Google API client
-    function handleClientLoad() {
-      gapi.load('client', initClient);
-        console.log('dashboard.js: gapi carregado.');
-    }
 
-    async function initClient() {
-       try {
-             await gapi.client.init({
-               clientId: CLIENT_ID,
-                scope: SCOPES,
-           });
-              console.log('dashboard.js: gapi inicializado.');
-             gapiInited = true;
-           } catch(error){
-            console.error("dashboard.js: Erro ao inicializar a API do Google", error);
-         }
-    };
     async function signIn(){
             console.log('dashboard.js: Iniciando a função signIn()...');
             if(gapiInited === false) {
@@ -79,8 +62,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     } catch (error){
           console.error("dashboard.js: Erro ao inicializar o Firebase:", error);
     }
-    handleClientLoad();
-    await initClient();
+   
     
     const logoutBtn = document.getElementById('logoutBtn');
         logoutBtn.addEventListener('click', async (e) => {
@@ -106,7 +88,7 @@ document.addEventListener("DOMContentLoaded", async () => {
              if (href === "#controle-evasao"){
                    window.open("https://forms.office.com/r/fBNbhiYfsb", "_blank");
                 } else if (href === "#enviar-documentos"){
-                    const signInSuccess = await signIn();
+                     const signInSuccess = await signIn();
                      if(signInSuccess) {
                         console.log('dashboard.js: signIn concluido, redirecionando para enviar-documentos.html');
                           window.location.href = 'enviar-documentos.html';
--- enviar-documentos.js
+++ enviar-documentos.js
@@ -17,27 +17,58 @@ const firebaseConfig = {
   const CLIENT_ID = '1067241825747-5i4mt2ud9m4q3qos8s0hn2ar4jgg256m.apps.googleusercontent.com';
   // google drive API scope.
   const SCOPES = 'https://www.googleapis.com/auth/drive.file';
+let tokenClient;
+let gapiInited = false;
+// Initialize the Google API client
+function handleClientLoad() {
+gapi.load('client', initClient);
+  console.log('enviar-documentos.js: gapi carregado.');
+}
+   // Initialize the client
+async function initClient() {
+ try{
+    await gapi.client.init({
+        clientId: CLIENT_ID,
+         scope: SCOPES,
+    });
+        console.log('enviar-documentos.js: gapi inicializado com sucesso.');
+        gapiInited = true;
+ } catch (error){
+      console.error("enviar-documentos.js: Erro ao inicializar a API do Google", error);
+ }
+};
 
-    let tokenClient;
-    let gapiInited = false;
-    console.log('enviar-documentos.js: Iniciando a API do Google...');
-     // Initialize the Google API client
-    function handleClientLoad() {
-          gapi.load('client', initClient);
-          console.log('enviar-documentos.js: gapi carregado.');
+async function signIn(){
+        console.log('enviar-documentos.js: Iniciando a função signIn()...');
+        if(gapiInited === false) {
+          alert('enviar-documentos.js: A api do google não foi inicializada, tente novamente!');
+           return;
+        }
+        console.log('enviar-documentos.js: gapiInited === true.');
+       tokenClient = google.accounts.oauth2.initTokenClient({
+         client_id: CLIENT_ID,
+         scope: SCOPES,
+         callback: '', // workaround for gapi issue
+      });
+       console.log('enviar-documentos.js: tokenClient criado.');
+      try{
+         await new Promise(resolve =>{
+           tokenClient.callback = async (resp) => {
+            if (resp.error !== undefined) {
+               throw (resp);
+            }
+                resolve(resp);
+          };
+          tokenClient.requestAccessToken();
+            console.log('enviar-documentos.js: accessToken requisitado com sucesso.');
+       });
+       } catch(error){
+          alert("enviar-documentos.js: Houve um problema ao fazer o login com o google, tente novamente");
+         console.error('enviar-documentos.js: Erro ao fazer login:', error);
+       }
     }
-     // Initialize the client
-    async function initClient() {
-         await gapi.client.init({
-           clientId: CLIENT_ID,
-           scope: SCOPES,
-         });
-           console.log('enviar-documentos.js: gapi inicializado com sucesso.');
-           gapiInited = true;
-    };
-   initClient();
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 // Inicializar Firebase
     try{
         console.log('enviar-documentos.js: Inicializando o Firebase...');
@@ -48,6 +79,9 @@ document.addEventListener('DOMContentLoaded', () => {
 } catch (error){
         console.error("enviar-documentos.js: Erro ao inicializar o Firebase:", error);
 }
+ handleClientLoad();
+ await initClient();
+ await signIn();
 
 const documentForm = document.getElementById('documentForm');
 const statusDiv = document.getElementById('status');
@@ -78,7 +112,7 @@ documentForm.addEventListener('submit', async (e) => {
                     timestamp: new Date(),
                 });
                statusDiv.textContent = 'Documento enviado com sucesso!';
-                console.log('enviar-documentos.js: Dados enviados para o firestore com sucesso.');
+               console.log('enviar-documentos.js: Dados enviados para o firestore com sucesso.');
                 documentForm.reset();
              } else{
                  statusDiv.textContent = 'Erro ao fazer upload do arquivo.';
